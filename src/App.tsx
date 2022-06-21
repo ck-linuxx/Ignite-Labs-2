803f296a1400bac2fcb9439608f726afb0536711
ck-linuxx <amorimClark@gmail.com>
2022-06-21 19:06:26 -0300

    master

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id 
+      title
+    }
+  }
+`
+
+interface ILesson {
+  id: string;
+  title: string;
+}
+
+function App() {
+  const { data } = useQuery<{ lessons: ILesson[] }>(GET_LESSONS_QUERY)
+
+  console.log(data)
+
+  return (
+    <ul>
+      { data?.lessons.map(lesson => { 
+        return <li key={lesson.id}>{ lesson.title }</li>
+       }) }
+    </ul>
+  )
+}
+
+export default App
